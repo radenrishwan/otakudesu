cb555cf256dfa5301336546b1d5b944ebf39c4b2
radenrishwan <radenrishwan@gmail.com>
2024-07-02 08:17:00 +0700

    add comment on endpoint

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 
+// TODO: you need to change api endpoint
+// you can get api from https://github.com/radenrishwan/otakudesu-api
 const endpoint = 'https://easy-gold-caiman-wig.cyclic.app/api';
 
 final dio = Dio()
